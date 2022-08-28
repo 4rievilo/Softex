a75e1f2cd87349cff6493333edf04a723014a1ac
Adalberto Medeiros Rodrigues de Oliveira <adalberto.medeiros444@gmail.com>
2022-08-28 12:09:55 -0300

    Bubble_2

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,12 +1,13 @@
 import random
 
-v = random.sample(range(9999), 10)
-print(f"{v}")
+c = 1
+v = random.sample(range(999), 10)
+print(f"{v}\n")
 for a in range(10):
     for b in range(9):
         if v[b] > v[b+1]:
             t = v[b]
             v[b] = v[b+1]
             v[b+1] = t
-for a in range(10):
-    print(f"{a+1} - {v[a]}")
+    print(f"Passo {c} - {v}")
+    c = c+1
